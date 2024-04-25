87e642b8ca5d4e2a7bf1a23d59d98b3a69654953
CHILI ERI RITHVIK <chiliveririthvik444@gmail.com>
2024-04-24 20:23:07 -0400

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const res = await axios.post(
-        "http://localhost:8080/api/v1/user/login",
+        "https://student-managment-system-5.onrender.com/api/v1/user/login",
         { email, password },
         {
           withCredentials: true,
